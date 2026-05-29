042026fb76243488f7d451168ed6482678fb2fc9
wwz-1017 <wwz7300@163.com>
2026-05-29 17:27:48 +0100

    Add Cloudflare deployment guide

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -47,7 +47,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1780060513354,
+    __BUILD_TIMESTAMP_MS__: 1780071640059,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
